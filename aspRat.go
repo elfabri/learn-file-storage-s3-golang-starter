f319178f020e0962700eb3f564651d08c6cf1549
elfabri <navarro.fabricio.nahuel@gmail.com>
2025-02-12 00:39:37 -0300

    aspect ratio calc

---
+++ aspRat.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"os/exec"
+)
+
+func GetVideoAspectRatio(filePath string) (string, error) {
+    var out bytes.Buffer
+    cmd := exec.Command("ffprobe", "-v", "error", "-print_format", "json", "-show_streams", filePath)
+    cmd.Stdout = &out
+
+    err := cmd.Run()
+    if err != nil {
+        return "", fmt.Errorf("Invalid Path: %s; error: %v\n", filePath, err)
+    }
+
+    type values struct {
+        Height float32 `json:"height"`
+        Width float32 `json:"width"`
+    }
+
+    type streams struct {
+        V []values `json:"streams"`
+    }
+
+    var s streams
+
+    err = json.Unmarshal(out.Bytes(), &s)
+    if err != nil {
+        return "", fmt.Errorf("Error unmarshalling video aspect ratio: %v\n", err)
+    }
+
+    aspRat := float32(s.V[0].Width) / float32(s.V[0].Height)
+    horizontal := float32(16) / float32(9)          // 1.7777
+    vertical := float32(9) / float32(16)            // 0.5625
+    tol := float32(0.01)
+
+    if aspRat <= (horizontal + tol) && aspRat >= (horizontal - tol) {
+        return "landscape", nil
+    }
+    if aspRat <= (vertical + tol) && aspRat >= (vertical - tol) {
+        return "portrait", nil
+    }
+
+    return "other", nil
+}
---
+++ aspRat_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestThis(t *testing.T) {
+    fmt.Println("Running aspect ratio test:")
+    fileP1 := "./samples/boots-video-horizontal.mp4"
+    fileP2 := "./samples/boots-video-vertical.mp4"
+
+    aspRat, err := GetVideoAspectRatio(fileP1)
+    if err != nil {
+        t.Errorf("Unspected error: %v\n", err)
+    }
+
+    if aspRat != "landscape" {
+        t.Errorf("should have been landscape: %v\n", aspRat)
+    }
+
+    aspRat, err = GetVideoAspectRatio(fileP2)
+    if err != nil {
+        t.Errorf("Unspected error: %v\n", err)
+    }
+
+    if aspRat != "portrait" {
+        t.Errorf("should have been portrait: %v\n", aspRat)
+    }
+}
